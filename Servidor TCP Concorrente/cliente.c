1df9dffc282bd0aeff4e511e0fc3ecd04c784dad
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2022-09-27 19:36:54 -0300

    Implemented previus codes

---
+++ Servidor TCP Concorrente/cliente.c
@@ -0,0 +1,87 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAXLINE 4096
+#define MAXCHAR 1000
+
+int main(int argc, char **argv) {
+    int    sockfd, n;
+    char   recvline[MAXLINE + 1];
+    char   error[MAXLINE + 1];
+    unsigned int port;
+    char   ip[16];
+    char   msg[MAXCHAR];
+    struct sockaddr_in servaddr;
+    socklen_t nAddrLen;
+    nAddrLen = sizeof(struct sockaddr_in);
+    
+
+    if (argc != 2) {
+        strcpy(error,"uso: ");
+        strcat(error,argv[0]);
+        strcat(error," <IPaddress>");
+        perror(error);
+        exit(1);
+    }
+
+    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket error");
+        exit(1);
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port   = htons(2022);
+    
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+        perror("inet_pton error");
+        exit(1);
+    }
+
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        perror("connect error");
+        exit(1);
+    }
+
+    // Get Ip and Port of Server
+    getsockname(sockfd, (struct sockaddr *) &servaddr, &nAddrLen);
+    inet_ntop(AF_INET, &servaddr.sin_addr, ip, sizeof(ip));
+    port = ntohs(servaddr.sin_port);
+    printf("IP: %s\n", ip);
+    printf("Porta local: %u\n", port);
+
+    while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+        recvline[n] = 0;
+        if (fputs(recvline, stdout) == EOF) {
+            perror("fputs error");
+            exit(1);
+        }
+
+        // Read msg
+        printf("Enter with message: ");
+        //scanf("%s", msg);
+        fgets(msg, MAXCHAR, stdin);
+
+        // Send msg
+        if(send(sockfd, msg, strlen(msg), 0) < 0) {
+            perror("send error");
+            exit(1);
+        }
+    }
+
+    if (n < 0) {
+        perror("read error");
+        exit(1);
+    }
+
+    exit(0);
+}
---
+++ Servidor TCP Concorrente/servidor.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+
+#define LISTENQ 10
+#define MAXDATASIZE 100
+
+int main (int argc, char **argv) {
+    int    listenfd, connfd;
+    struct sockaddr_in servaddr;
+    char   buf[MAXDATASIZE];
+    time_t ticks;
+    char   ipp[16];
+    unsigned int portT;
+    char client_message[1000];
+    int read_size;
+    
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    servaddr.sin_port        = htons(2222);   
+    
+    
+
+    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(listenfd, LISTENQ) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    struct sockaddr_in servaddr2;
+
+    for ( ; ; ) {
+      if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
+        perror("accept");
+        exit(1);
+        }
+	
+		//
+		bzero(&servaddr2, sizeof(servaddr2));
+		socklen_t len = sizeof(servaddr2);
+		getpeername(connfd, (struct sockaddr * )&servaddr2, &len);
+		inet_ntop(AF_INET, &servaddr2.sin_addr, ipp, sizeof(ipp));
+		portT = ntohs(servaddr2.sin_port);
+		printf("IP: %s\n", ipp);
+		printf("Porta: %u\n", portT);
+		//
+
+        ticks = time(NULL);
+        snprintf(buf, sizeof(buf), "Hello from server!\nTime: %.24s\r\n", ctime(&ticks));
+        write(connfd, buf, strlen(buf));
+
+        read(connfd, client_message, 1000);
+        printf("recieved message: %s \n", client_message);
+        close(connfd);
+
+    }
+    return(0);
+}
