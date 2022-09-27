8c45b66d50ff8ede41411a990795e17205c512b7
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2022-09-27 20:00:14 -0300

    update server

--- Servidor TCP Concorrente/servidor.c
+++ Servidor TCP Concorrente/servidor.c
@@ -8,20 +8,25 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define LISTENQ 10
 #define MAXDATASIZE 100
+#define MAXCHAR 1000
 
 int main (int argc, char **argv) {
     int    listenfd, connfd;
     struct sockaddr_in servaddr;
+    struct sockaddr_in servaddr2;
     char   buf[MAXDATASIZE];
+    char   ip[16];
+    char   received_msg[MAXCHAR];
     time_t ticks;
-    char   ipp[16];
-    unsigned int portT;
-    char client_message[1000];
-    int read_size;
-    
+    unsigned int port;
+    socklen_t nAddr2Len;
+    nAddr2Len = sizeof(struct sockaddr_in);
+
+
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
@@ -29,10 +34,8 @@ int main (int argc, char **argv) {
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family      = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    servaddr.sin_port        = htons(2022);   
-    
-    
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(2022);
 
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
         perror("bind");
@@ -44,33 +47,30 @@ int main (int argc, char **argv) {
         exit(1);
     }
 
-    struct sockaddr_in servaddr2;
-
     for ( ; ; ) {
       if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
         perror("accept");
         exit(1);
         }
-	
-		//
-		bzero(&servaddr2, sizeof(servaddr2));
-		socklen_t len = sizeof(servaddr2);
-		getpeername(connfd, (struct sockaddr * )&servaddr2, &len);
-		inet_ntop(AF_INET, &servaddr2.sin_addr, ipp, sizeof(ipp));
-		portT = ntohs(servaddr2.sin_port);
-		printf("IP: %s\n", ipp);
-		printf("Porta: %u\n", portT);
-		//
+
+        // Get Ip and Port of Client
+        getpeername(connfd, (struct sockaddr * )&servaddr2, &nAddr2Len);
+		inet_ntop(AF_INET, &servaddr2.sin_addr, ip, sizeof(ip));
+		port = ntohs(servaddr2.sin_port);
+		printf("IP: %s\n", ip);
+		printf("Porta: %u\n", port);
 
         ticks = time(NULL);
         snprintf(buf, sizeof(buf), "Hello from server!\nTime: %.24s\r\n", ctime(&ticks));
         write(connfd, buf, strlen(buf));
 
-        read(connfd, client_message, 1000);
-        printf("recieved message: %s \n", client_message);
-        sleep(6);
-        close(connfd);
+        // Read message
+        read(connfd, received_msg, MAXCHAR);
+        printf("Message: %s \n", received_msg);
+        *received_msg = '\0';
 
+        sleep(8);
+        close(connfd);
     }
     return(0);
 }
